ce15030c0a69ebb9c671bbcdb8ebeb4ad0888dbf
Sergey <31635516+8m8s8g8@users.noreply.github.com>
2022-11-22 15:16:11 +0300

    большее число

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+
+int a = new Random().Next(0,10), b = new Random().Next(0,10);
+Console.WriteLine(a);
+Console.WriteLine(b);
+int max = a;
+if(a > b) max = a;
+if(a < b) max = b;
+Console.WriteLine ("большее число ");
+Console.Write(max);
